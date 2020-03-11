641477bc437906a3ad66670319092f604b4cb892
lourencofelipe <felipe.pucsc@gmail.com>
2020-03-11 08:40:11 -0300

    Creating TF graph

--- RL.py
+++ RL.py
@@ -38,10 +38,27 @@ def createGraph():
 
     #Fouth convolutional layer
     W_fc4 = tf.Variable(tf.zeros([784, ACTIONS]))
-    b_fc5 = tf.Variable(tf.zeros([784]))
+    b_fc4 = tf.Variable(tf.zeros([784]))
 
     #Last layer
     W_fc5 = td.Variable(tf.zeros([784, ACTIONS]))
     b_fc5 = tf.Variable(tf.zeros([ACTIONS]))
-    
 
+
+    #Input for pixel data
+    s = tf.placeholder("float", [None, 84, 84, 4])
+
+    #Computes rectified linear unit activation function on a 2-D convolution given 4-D input and filter tensors
+    conv1 = tf.nn.relu(tf.nn.conv2d(s, W_conv1, strides = [1, 4, 4, 1], padding = "VALID") + b_conv1)
+
+    conv2 = tf.nn.relu(tf.nn.conv2d(conv1, W_conv2, strides = [1, 2, 2, 1], padding = "VALID") + b_conv2)
+
+    conv3 = tf.nn.relu(tf.nn.conv2d(conv2, W_conv3, strides = [1, 1, 1, 1], padding = "VALID") + b_conv3)
+
+    conv3_flat = tf.reshape(conv3, [-1, 3136])
+
+    fc4 = tf.nn.relu(tf.matmul(conv3_flat, W_fc4) + b_fc4)
+    
+    fc5 = tf.matmul(fc4, W_fc5) + b_fc5
+
+    return s, fc5
